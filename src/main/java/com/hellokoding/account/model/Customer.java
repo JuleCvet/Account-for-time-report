0788033575b030a0d3bcb90404f363f376777017
julij <julijanacvetkovska@gmail.com>
2018-04-05 13:48:32 +0200

    cleaning the code

--- src/main/java/com/hellokoding/account/model/Customer.java
+++ src/main/java/com/hellokoding/account/model/Customer.java
@@ -1,14 +1,10 @@
 package com.hellokoding.account.model;
 
-import java.util.Set;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "customer")
