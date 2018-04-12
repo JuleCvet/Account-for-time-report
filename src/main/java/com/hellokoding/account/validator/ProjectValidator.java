868733b2f64a750d72fccf903c21c4c18bdac229
julij <julijanacvetkovska@gmail.com>
2018-04-12 10:34:24 +0200

    Method for validating characters in Project

--- src/main/java/com/hellokoding/account/validator/ProjectValidator.java
+++ src/main/java/com/hellokoding/account/validator/ProjectValidator.java
@@ -1,5 +1,8 @@
 package com.hellokoding.account.validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -19,7 +22,17 @@ public class ProjectValidator implements Validator {
 	public boolean supports(Class<?> aClass) {
 		return Project.class.equals(aClass);
 	}
-
+	
+	public boolean checkChars(String s) {
+		Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(s);
+		boolean b = m.find();
+		if (b) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	@Override
 	public void validate(Object o, Errors errors) {
@@ -28,12 +41,19 @@ public class ProjectValidator implements Validator {
 		
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "projectName", "NotEmpty");
 		if(project.getProjectName().length() < 6 || project.getProjectName().length() > 32) {
-			errors.reject("projectName", "Size.projectForm.projectName");
+			errors.rejectValue("projectName", "Size.projectForm.projectName");
+		
+		if(checkChars(project.getProjectName())) {
+			errors.rejectValue("projectName", "Chars.projectForm.projectName");
+		}	
 		
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "description", "NotEmpty");
 		if(project.getDescription().length() < 6  || project.getDescription().length() > 32) {
-			errors.reject("description", "Size.projectForm.description");
-		}	
+			errors.rejectValue("description", "Size.projectForm.description");
+		}
+		if (checkChars(project.getDescription())) {
+			errors.rejectValue("description", "Chars.projectForm.description");
+		}
 	/*	if(projectService.findByProjectname(project.getProjectName() != null)) {
 			errors.rejectValue("projectName", "Duplicate.projectForm.projectName");
 			}*/
